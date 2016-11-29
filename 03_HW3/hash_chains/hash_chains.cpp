abadbd5ebc78084f3a3a3ef4033af6605e468d2e
Andrey Andreyev <andreyev.andrey@gmail.com>
2016-11-29 00:03:52 -0500

    created the linked list for chaining

--- 03_HW3/hash_chains/hash_chains.cpp
+++ 03_HW3/hash_chains/hash_chains.cpp
@@ -12,21 +12,90 @@ struct Query {
     size_t ind;
 };
 
+struct node{
+	node():pNext(NULL){}
+	string myString;
+	node* pNext;
+};
+
+struct HEAD{
+	HEAD():pNext(NULL){}
+	node* pNext;
+};
+
+class LinkedList{
+	private:
+	HEAD* myHead;
+	bool searchString(node* pN, string const &str){
+		if (pN->myString==str){
+			return true;
+		}
+		if (pN->pNext==NULL){
+			return false
+		}
+
+		return searchString(pN->pNext,str);
+	}
+
+	public:
+	LinkedList(){
+		cout<<"Inside the constructor\n"
+		myHead=new HEAD;
+		myHead->pNext=NULL;
+	}
+	~LinkedList(){
+		node* currNode=myHead->pNext;
+		while(currNode!=NULL){
+			node* tmp=currNode->pNext;
+			delete currNode;
+			currNode=tmp;
+		}
+		delete myHead;
+	}
+	bool searchString(string const & s){
+		if (myHead->pNext==NULL){
+			return false;
+		}else{
+			return searchString(myHead->pNext,s);
+		}
+	}
+
+	void insertString(string const & s){
+		if (this->searchString(s)){
+			return;
+		}else{
+			node* oldPoint=myHead->pNext;
+			myHead->pNext=new node;
+			myHead->pNext.myString=s;
+			myHead->pNext->pNext=oldPoint;
+			oldPoint=NULL;
+		}
+	}
+	
+
+};
+
 class QueryProcessor {
     int bucket_count;
     // store all strings in one vector
-    vector<string> elems;
+    vector<LinkedList> buckets;
     size_t hash_func(const string& s) const {
         static const size_t multiplier = 263;
         static const size_t prime = 1000000007;
         unsigned long long hash = 0;
         for (int i = static_cast<int> (s.size()) - 1; i >= 0; --i)
-            hash = (hash * multiplier + s[i]) % prime;
-        return hash % bucket_count;
+            hash = ((hash * multiplier + s[i]) % prime) % bucket_count;
+        return hash;
     }
 
 public:
-    explicit QueryProcessor(int bucket_count): bucket_count(bucket_count) {}
+    QueryProcessor(int bucket_count){
+		this->bucket_count=bucket_count;
+		this->buckets.resize(bucket_count);
+		for (int i=0;i<bucket_count;i++){
+			buckets[i]
+		}
+	}
 
     Query readQuery() const {
         Query query;
@@ -38,37 +107,7 @@ public:
         return query;
     }
 
-    void writeSearchResult(bool was_found) const {
-        std::cout << (was_found ? "yes\n" : "no\n");
-    }
-
-    void processQuery(const Query& query) {
-        if (query.type == "check") {
-            // use reverse order, because we append strings to the end
-            for (int i = static_cast<int>(elems.size()) - 1; i >= 0; --i)
-                if (hash_func(elems[i]) == query.ind)
-                    std::cout << elems[i] << " ";
-            std::cout << "\n";
-        } else {
-            vector<string>::iterator it = std::find(elems.begin(), elems.end(), query.s);
-            if (query.type == "find")
-                writeSearchResult(it != elems.end());
-            else if (query.type == "add") {
-                if (it == elems.end())
-                    elems.push_back(query.s);
-            } else if (query.type == "del") {
-                if (it != elems.end())
-                    elems.erase(it);
-            }
-        }
-    }
-
-    void processQueries() {
-        int n;
-        cin >> n;
-        for (int i = 0; i < n; ++i)
-            processQuery(readQuery());
-    }
+    
 };
 
 int main() {
