eaea5b0a263e2e1fea4f91567d388af7ec9da73a
Andrey Andreyev <andreyev.andrey@gmail.com>
2016-12-15 00:18:19 -0500

    inserts seem to work

--- 04_HW4/set_range_sum/splay_tree.cpp
+++ 04_HW4/set_range_sum/splay_tree.cpp
@@ -1,17 +1,16 @@
 #include<iostream>
-#include<string>
 
 using std::cout;
-
-using std::string;
+using std::endl;
 struct Node{
-	Node: parent(NULL),left(NULL),right(NULL),key(0),sum(0);{}
+	Node(): parent(NULL),left(NULL),right(NULL),key(0),sum(0){}
+
 	Node* parent;
 	Node* left;
 	Node* right;
 	int key;
 	long long sum;
-
+	
 };
 
 
@@ -40,7 +39,7 @@ class SplayTree{
 	}	
 	void rotate(Node* curNode){
 		Node* parent=curNode->parent;
-		if (parent==NULL) {return};
+		if (parent==NULL) {return;}
 		Node* grandParent=parent->parent;
 		Node* tmp=NULL;
 		//Right rotation case
@@ -82,22 +81,24 @@ class SplayTree{
 		//if curNode is parent's left child,
 		//the parent has to be Grandparent's left child
 		//Vise verse with right child case 
-		if (grandParent->left==parent and parent->left==curNode){
-			rotate(curNode->parent);
-			rotate(curNode);
-		}else if(grandParent->right ==parent and parent->right==curNode){
-			rotate(curNode->parent);
-			rotate(curNode);
-		}
+		if (grandParent!=NULL){
+			if (grandParent->left==parent and parent->left==curNode){
+				rotate(curNode->parent);
+				rotate(curNode);
+			}else if(grandParent->right ==parent and parent->right==curNode){
+				rotate(curNode->parent);
+				rotate(curNode);
+			}
 
-		//zig zag case
-	 	if (grandParent->left==parent and parent->right==curNode){
-			rotate(curNode);
-			rotate(curNode);
-		}else if(grandParent->right==parent and parent->left==curNode){
-			rotate(curNode);
-			rotate(curNode);
-		}		
+			//zig zag case
+	 		if (grandParent->left==parent and parent->right==curNode){
+				rotate(curNode);
+				rotate(curNode);
+			}else if(grandParent->right==parent and parent->left==curNode){
+				rotate(curNode);
+				rotate(curNode);
+			}		
+		}
 		splay(curNode);
 	}
 
@@ -147,40 +148,38 @@ class SplayTree{
 	}
 	//this* is always the left tree
 	SplayTree* split(int key,SplayTree* leftTree){
-		Node* foudNode=find(leftTree->root,key);
+		Node* foundNode=find(leftTree->root,key);
 		splay(foundNode);
 		if (key<leftTree->root->key){
 			if (leftTree->root->left==NULL){
-				return;
+				return NULL;
 			}
 			SplayTree* rightTree=new SplayTree;
 			rightTree->root=foundNode;
-			leftTree->root=rightTree->root->left;
-			leftTree->root->parent=NULL;
+			leftTree->setRoot(rightTree->root->left);
 			rightTree->root->left==NULL;
 			update(rightTree->root);
             return rightTree;
 		}else{
 			if (this->root->right==NULL){
-				return;
+				return NULL;
 			}
 			SplayTree* rightTree=new SplayTree;
-			rightTree->root=leftTree->root->right;
-			rightTree->root->prant=NULL;
+			rightTree->setRoot(leftTree->root->right);
 			leftTree->root->right=NULL;
 			update(leftTree->root);
             return rightTree;
 		}
 	}
 
-	void merge(SplayTree*& leftTree, SplayTree* & rightTree){
+	void merge(SplayTree* leftTree, SplayTree* rightTree){
 		int largest=2147483647;
 		Node* foundNode=NULL;
 		foundNode=leftTree->find(leftTree->root,largest);
 		leftTree->splay(foundNode);
 		leftTree->root->right=rightTree->root;
-		update(left->root);
-		right->root=NULL;
+		update(leftTree->root);
+		rightTree->root=NULL;
 		delete rightTree;
 		rightTree=NULL;
 	}
@@ -192,6 +191,9 @@ class SplayTree{
 	SplayTree(Node* curNode){
 		root=curNode;
 		root->parent=NULL;
+	}
+	Node* getRoot(){
+		return root;
 	}
 	void insert(int key){
 		Node* newNode=NULL;
@@ -205,7 +207,6 @@ class SplayTree{
 		}
 		SplayTree* rightTree=NULL;
 		rightTree=split(key,this);
-		
 		if (rightTree==NULL){
 			if (this->root->right==NULL and this->root->key!=key){
 				newNode=new Node;
@@ -223,6 +224,7 @@ class SplayTree{
 			return;
 		}
 		if (this->root->right==NULL and this->root->key!=key){
+			//cout<<"why am I in here?"<<endl
 			newNode=new Node;
 			newNode->key=key;
 			newNode->sum=key;
@@ -230,6 +232,7 @@ class SplayTree{
 			update(this->root);
 			merge(this,rightTree);
 		}else if(rightTree->root->left==NULL and rightTree->root->key!=key){
+			//cout<<"shouldn't be here either"<<endl;
 			newNode=new Node;
 			newNode->key=key;
 			newNode->sum=key;
@@ -243,6 +246,7 @@ class SplayTree{
 	bool find(int key){
 		Node* foundNode=find(root,key);
 		if (foundNode->key==key){
+			cout<<"Fuck yeah\n";
 			return true;
 		}else{
 			return false;
@@ -252,7 +256,7 @@ class SplayTree{
         return split(key,this);
     }
 
-    void merge(splayTree* mergeTree){
+    void merge(SplayTree* mergeTree){
         merge(this,mergeTree);
     }
 	int range_sum(int l, int r){
@@ -270,3 +274,21 @@ class SplayTree{
 	}
 
 };
+
+int main(){
+	SplayTree* test=new SplayTree;
+	cout<<"Insert: "<<3<<endl;
+	test->insert(3);
+	cout<<"Insert: "<<2<<endl;
+	test->insert(2);
+	cout<<"Insert: "<<1<<endl;
+	test->insert(1);
+
+	Node* root=test->getRoot();
+	cout<<root->key<<endl;
+	cout<<root->left->key<<endl;
+	cout<<root->right->key<<endl;
+	cout<<root->sum<<endl;
+	
+return 0;
+}
